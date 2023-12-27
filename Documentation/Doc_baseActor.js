cea482c98f264f4a233ce6c2aefb4deb5d356f4e
Virgil <virgilvancini@gmail.com>
2023-12-27 15:05:28 +0100

    starting to define the hierarchy of actors

---
+++ Documentation/Doc_baseActor.js
@@ -0,0 +1,33 @@
+/**
+ * The categories for an actor should be :
+ * 
+ *  - actor
+ *  - pawn
+ *  - character
+ * 
+ *  A character should be an actor capable of movement, interaction, combat , dialogue and more complex type of actions.
+ *  It should be the type of actor with the most agency possible and the players should use characters instead of pawns
+ *  or actors.
+ * 
+ *      * Examples of characters : 
+ *              - humans / humanoids
+ *              - animals / beasts
+ *              - NPCs, Mobs, Players
+ * 
+ *  A pawn should be an actor capable of movement and combat. It should not be able to do more complex actions such as forging,
+ *  investigating or dialogue. Pawns are rarely under the control of players but it can happen such owning a vehicle.
+ *  Since a pawn is capable of combat, it should have an anatomy.
+ * 
+ *      * Examples of pawns : 
+ *              - turrets / siege weapons
+ *              - traps
+ *              - vehicles
+ * 
+ * An actor is incapable of movement or combat therefore it is void of any anatomy. Actors are the most unique since they are 
+ * tailored to their functions and each perform their unique task.
+ * 
+ *      * Examples of actors : 
+ *              - chests / loot bags
+ *              - work stations / altars
+ *              - country
+ */
